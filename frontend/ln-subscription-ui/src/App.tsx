6fe8de33ab1cd69687821e49a5baa75850d1e0ce
Adam Sargent <adamcsargent@gmail.com>
2022-11-23 16:13:17 -0500

    Create ts React app

---
+++ frontend/ln-subscription-ui/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="app">
+			<header className="main-header">
+			</header>
+			<main className="main-content">
+				Hello world!
+			</main>
+			<footer className="main-footer"></footer>
+		</div>
+	);
+}
+
+export default App;
